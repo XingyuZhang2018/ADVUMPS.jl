4aa87fc0e9987fbe2b1358e4300a3631bc986a1c
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2021-04-19 09:46:48 +0800

    correct dFL and dFR and add annotations for autodiff.jl

--- src/autodiff.jl
+++ src/autodiff.jl
@@ -9,12 +9,8 @@ using KrylovKit
 @Zygote.nograd printstyled
 
 # patch since it's currently broken otherwise
-# @Zygote.adjoint function Base.typed_hvcat(::Type{T}, rows::Tuple{Vararg{Int}}, xs::S...) where {T,S}
-#     Base.typed_hvcat(T,rows, xs...), ȳ -> (nothing, nothing, permutedims(ȳ)...)
-# end
-
 function ChainRulesCore.rrule(::typeof(Base.typed_hvcat), ::Type{T}, rows::Tuple{Vararg{Int}}, xs::S...) where {T,S}
-    y = Base.typed_hvcat(T,rows, xs...)
+    y = Base.typed_hvcat(T, rows, xs...)
     function back(ȳ)
         return NO_FIELDS, NO_FIELDS, NO_FIELDS, permutedims(ȳ)...
     end
@@ -31,89 +27,163 @@ function ChainRulesCore.rrule(::typeof(LinearAlgebra.norm), A::AbstractArray)
     return n, back
 end
 
-function ChainRulesCore.rrule(::typeof(leftenv),AL::AbstractArray{T}, M::AbstractArray{T}, FL::AbstractArray{T}; kwargs...) where {T}
+"""
+    ChainRulesCore.rrule(::typeof(leftenv), AL::AbstractArray{T}, M::AbstractArray{T}, FL::AbstractArray{T}; kwargs...) where {T}
+
+```
+          ┌──  AL ──┐ 
+          │    │    │ 
+dM   =   FL ──   ── ξl
+          │    │    │ 
+          └──  AL ──┘ 
+
+          ┌──     ──┐         ┌──  AL ──┐ 
+          │    │    │         │    │    │ 
+dAL  =   FL ── M ── ξl   +   FL ── M ── ξl
+          │    │    │         │    │    │ 
+          └──  AL ──┘         └──     ──┘ 
+```
+"""
+function ChainRulesCore.rrule(::typeof(leftenv), AL::AbstractArray{T}, M::AbstractArray{T}, FL::AbstractArray{T}; kwargs...) where {T}
     λ, FL = leftenv(AL, M, FL; kwargs...)
     function back((dλ, dFL))
-        ξl = rand(T,size(FL))
-        ξl = ein"ηpβ,βaα,csap,γsα -> ηcγ"(AL,ξl,M,conj(AL)) - λ .* ξl
-        ξl,info = linsolve(FR -> ein"ηpβ,βaα,csap,γsα -> ηcγ"(AL,FR,M,conj(AL)), dFL, ξl, -λ, 1; kwargs...)
-        dAL = -ein"γcη,csap,γsα,βaα -> ηpβ"(FL,M,conj(AL),ξl) - ein"γcη,csap,ηpβ,βaα -> γsα"(FL,M,AL,ξl)
-        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(FL,AL,conj(AL),ξl)
+        ξl = rand(T, size(FL))
+        ξl = ein"ηpβ,βaα,csap,γsα -> ηcγ"(AL, ξl, M, conj(AL)) - λ .* ξl
+        ξl, info = linsolve(FR -> ein"ηpβ,βaα,csap,γsα -> ηcγ"(AL, FR, M, conj(AL)), permutedims(dFL, (3, 2, 1)), ξl, -λ, 1; kwargs...)
+        dAL = -ein"γcη,csap,γsα,βaα -> ηpβ"(FL, M, conj(AL), ξl) - ein"γcη,csap,ηpβ,βaα -> γsα"(FL, M, AL, ξl)
+        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(FL, AL, conj(AL), ξl)
         # @show info ein"abc,abc ->"(FL,ξl)[] ein"γpη,γpη -> "(FL,dFL)[]
         return NO_FIELDS, dAL, dM, NO_FIELDS...
     end
     return (λ, FL), back
 end
 
-function ChainRulesCore.rrule(::typeof(rightenv),AR::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; kwargs...) where {T}
+"""
+    ChainRulesCore.rrule(::typeof(rightenv), AR::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; kwargs...) where {T}
+
+```
+          ┌──  AR ──┐ 
+          │    │    │ 
+dM   =   ξr ──   ── FR
+          │    │    │ 
+          └──  AR ──┘ 
+
+          ┌──     ──┐         ┌──  AR ──┐ 
+          │    │    │         │    │    │ 
+dAR  =   ξr ── M ── FR   +   ξr ── M ── FR
+          │    │    │         │    │    │ 
+          └──  AR ──┘         └──     ──┘
+```
+"""
+function ChainRulesCore.rrule(::typeof(rightenv), AR::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; kwargs...) where {T}
     λ, FR = rightenv(AR, M, FR; kwargs...)
     function back((dλ, dFR))
-        ξr = rand(T,size(FR))
-        ξr = ein"ηpβ,γcη,csap,γsα -> αaβ"(AR,ξr,M,conj(AR)) - λ .* ξr
-        ξr,info = linsolve(FL -> ein"ηpβ,γcη,csap,γsα -> αaβ"(AR,FL,M,conj(AR)), dFR, ξr, -λ, 1; kwargs...)
-        dAR = -ein"γcη,csap,γsα,βaα -> ηpβ"(ξr,M,conj(AR),FR) - ein"γcη,csap,ηpβ,βaα -> γsα"(ξr,M,AR,FR)
-        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(ξr,AR,conj(AR),FR)
+        ξr = rand(T, size(FR))
+        ξr = ein"ηpβ,γcη,csap,γsα -> αaβ"(AR, ξr, M, conj(AR)) - λ .* ξr
+        ξr, info = linsolve(FL -> ein"ηpβ,γcη,csap,γsα -> αaβ"(AR, FL, M, conj(AR)), permutedims(dFR, (3, 2, 1)), ξr, -λ, 1; kwargs...)
+        dAR = -ein"γcη,csap,γsα,βaα -> ηpβ"(ξr, M, conj(AR), FR) - ein"γcη,csap,ηpβ,βaα -> γsα"(ξr, M, AR, FR)
+        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(ξr, AR, conj(AR), FR)
         return NO_FIELDS, dAR, dM, NO_FIELDS...
     end
     return (λ, FR), back
 end
 
+"""
+    ChainRulesCore.rrule(::typeof(ACenv),AC::AbstractArray{T}, FL::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; 
+
+```
+          ┌──  AC ──┐ 
+          │    │    │ 
+dM   =   FL ──   ── FR
+          │    │    │ 
+          └──  ξ  ──┘ 
+
+          ┌──  AC ──┐ 
+          │    │    │ 
+dFL  =      ── M ── FR
+          │    │    │ 
+          └──  ξ  ──┘ 
+
+          ┌──  AC ──┐ 
+          │    │    │ 
+dFR  =   FL ── M ── 
+          │    │    │ 
+          └──  ξ  ──┘       
+```
+"""
 function ChainRulesCore.rrule(::typeof(ACenv),AC::AbstractArray{T}, FL::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; 
     kwargs...) where {T}
     λ, AC = ACenv(AC, FL, M, FR; kwargs...)
     function back((dλ, dAC))
-        ξ = rand(T,size(AC))
-        ξ = ein"αaγ,αsβ,asbp,ηbβ -> γpη"(FL,ξ,M,FR) - λ .* ξ
+        ξ = rand(T, size(AC))
+        ξ = ein"αaγ,αsβ,asbp,ηbβ -> γpη"(FL, ξ, M, FR) - λ .* ξ
         # b = dAC - AC .* ein"γpη,γpη -> "(AC,dAC)[]
-        ξ,info = linsolve(AC -> ein"αaγ,αsβ,asbp,ηbβ -> γpη"(FL,AC,M,FR), dAC, ξ, -λ, 1; kwargs...)
+        ξ, info = linsolve(AC -> ein"αaγ,αsβ,asbp,ηbβ -> γpη"(FL, AC, M, FR), dAC, ξ, -λ, 1; kwargs...)
         # @show info ein"abc,abc ->"(AC,ξ)[] ein"γpη,γpη -> "(AC,dAC)[]
-        dFL = -ein"ηpβ,βaα,csap,γsα -> ηcγ"(AC,FR,M,ξ)
-        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(FL,AC,ξ,FR)
-        dFR = -ein"ηpβ,γcη,csap,γsα -> αaβ"(AC,FL,M,ξ)
+        dFL = -ein"ηpβ,βaα,csap,γsα -> γcη"(AC, FR, M, ξ)
+        dM = -ein"γcη,ηpβ,γsα,βaα -> csap"(FL, AC, ξ, FR)
+        dFR = -ein"ηpβ,γcη,csap,γsα -> βaα"(AC, FL, M, ξ)
         return NO_FIELDS, NO_FIELDS, dFL, dM, dFR
     end
     return (λ, AC), back
 end
 
+"""
+    ChainRulesCore.rrule(::typeof(ACenv),AC::AbstractArray{T}, FL::AbstractArray{T}, M::AbstractArray{T}, FR::AbstractArray{T}; 
+
+```
+          ┌──  C  ──┐ 
+          │         │ 
+dFL  =      ─────── FR
+          │         │ 
+          └──  ξ  ──┘ 
+
+          ┌──  AC ──┐ 
+          │         │ 
+dFR  =   FL ─────── 
+          │         │ 
+          └──  ξ  ──┘       
+```
+"""
 function ChainRulesCore.rrule(::typeof(Cenv), C::AbstractArray{T}, FL::AbstractArray{T}, FR::AbstractArray{T}; kwargs...) where {T}
     λ, C = Cenv(C, FL, FR; kwargs...)
     function back((dλ, dC))
-        ξ = rand(T,size(C))
-        ξ = ein"αaγ,αβ,ηaβ -> γη"(FL,ξ,FR) - λ .* ξ
+        ξ = rand(T, size(C))
+        ξ = ein"αaγ,αβ,ηaβ -> γη"(FL, ξ, FR) - λ .* ξ
         # b = dC - C .* ein"γpη,γpη -> "(C,dC)[]
-        ξ,info = linsolve(C -> ein"αaγ,αβ,ηaβ -> γη"(FL,C,FR), dC, ξ, -λ, 1; kwargs...)
+        ξ, info = linsolve(C -> ein"αaγ,αβ,ηaβ -> γη"(FL, C, FR), dC, ξ, -λ, 1; kwargs...)
         # @show info ein"ab,ab ->"(C,ξ)[] ein"γp,γp -> "(C,dC)[]
-        dFL = -ein"ηβ,βaα,γα -> ηaγ"(C,FR,ξ)
-        dFR = -ein"ηβ,γcη,γα -> αcβ"(C,FL,ξ)
+        dFL = -ein"ηβ,βaα,γα -> γaη"(C, FR, ξ)
+        dFR = -ein"ηβ,γcη,γα -> βcα"(C, FL, ξ)
         return NO_FIELDS, NO_FIELDS, dFL, dFR
     end
     return (λ, C), back
 end
 
-#adjoint for QR factorization
-#https://journals.aps.org/prx/abstract/10.1103/PhysRevX.9.031041 eq.(5)
-function ChainRulesCore.rrule(::typeof(qrpos),A::AbstractArray{T,2}) where {T}
-    Q,R = qrpos(A)
-    function back((dQ,dR))
-        M = R * dR' - dQ'*Q
-        Rt = rand(T,size(R))
-        Rt,info = linsolve(x -> R * x, Matrix(I,size(R)), Rt, 0, 1)
+# adjoint for QR factorization
+# https://journals.aps.org/prx/abstract/10.1103/PhysRevX.9.031041 eq.(5)
+function ChainRulesCore.rrule(::typeof(qrpos), A::AbstractArray{T,2}) where {T}
+    Q, R = qrpos(A)
+    function back((dQ, dR))
+        M = R * dR' - dQ' * Q
+        Rt = rand(T, size(R))
+        Rt, info = linsolve(x -> R * x, Matrix(I, size(R)), Rt, 0, 1)
         dA = (dQ + Q * Symmetric(M, :L)) * (Rt)'
         return NO_FIELDS, dA
     end
-    return (Q,R), back
+    return (Q, R), back
 end
 
-function ChainRulesCore.rrule(::typeof(lqpos),A::AbstractArray{T,2}) where {T}
-    L,Q = lqpos(A)
-    function back((dL,dQ))
-        M = L' * dL - dQ*Q'
-        Lt = rand(T,size(L))
-        Lt,info = linsolve(x -> L * x, Matrix(I,size(L)), Lt, 0, 1)
+function ChainRulesCore.rrule(::typeof(lqpos), A::AbstractArray{T,2}) where {T}
+    L, Q = lqpos(A)
+    function back((dL, dQ))
+        M = L' * dL - dQ * Q'
+        Lt = rand(T, size(L))
+        Lt, info = linsolve(x -> L * x, Matrix(I, size(L)), Lt, 0, 1)
         dA = (Lt)' * (dQ + Symmetric(M, :L) * Q)
         return NO_FIELDS, dA
     end
-    return (L,Q), back
+    return (L, Q), back
 end
 
 @doc raw"
@@ -129,7 +199,7 @@ julia> TensorNetworkAD.num_grad(x -> x * x, 3) ≈ 6
 true
 ```
 "
-num_grad(f, K::Real; δ::Real = 1e-5) = (f(K+δ/2) - f(K-δ/2))/δ
+num_grad(f, K::Real; δ::Real=1e-5) = (f(K + δ / 2) - f(K - δ / 2)) / δ
 
 @doc raw"
     num_grad(f, K::AbstractArray; [δ = 1e-5])
@@ -143,9 +213,9 @@ julia> TensorNetworkAD.num_grad(tr, rand(2,2)) ≈ I
 true
 ```
 "
-function num_grad(f, a::AbstractArray; δ::Real = 1e-5)
+function num_grad(f, a::AbstractArray; δ::Real=1e-5)
     map(CartesianIndices(a)) do i
         foo = x -> (ac = copy(a); ac[i] = x; f(ac))
-        num_grad(foo, a[i], δ = δ)
+        num_grad(foo, a[i], δ=δ)
     end
 end
