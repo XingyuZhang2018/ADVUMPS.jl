65e7f9f984aaa60fa43a26abdd9481e2a7f1c4b2
qiyang <qiyang@mail.ustc.edu.cn>
2021-10-06 12:32:06 +0800

    update CUDA support

--- src/cuda_patch.jl
+++ src/cuda_patch.jl
@@ -14,8 +14,8 @@ using LinearAlgebra
 #helper functions to handle array types
 _mattype(x::Array{T}) where {T} = Matrix
 _mattype(x::CuArray{T}) where {T} = CuMatrix
-_mattype(x::Adjoint{T, CuArray{T, 2}}) where {T} = CuMatrix
-_mattype(x::Symmetric{T, CuArray{T, 2}}) where {T} = CuMatrix
+_mattype(x::Adjoint{T, CuArray{T, 2 ,B}}) where {T,B} = CuMatrix
+_mattype(x::Symmetric{T, CuArray{T, 2, B}}) where {T,B} = CuMatrix
 
 _arraytype(x::Array{T}) where {T} = Array
 _arraytype(x::CuArray{T}) where {T} = CuArray
